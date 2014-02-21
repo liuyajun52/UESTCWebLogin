d67d47f1ca5da42da985d3101322d92346ca9e53
liuyajun <liuyajun@localhost.localdomain>
2014-02-21 19:45:30 +0800

    增加记住账户和密码功能

--- src/com/blacklighting/uestcweblogin/MainActivity.java
+++ src/com/blacklighting/uestcweblogin/MainActivity.java
@@ -9,12 +9,19 @@ import com.blacklighting.uestcweblogin.internet.LogThread;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.preference.PreferenceManager;
 import android.app.Activity;
+import android.app.AlertDialog.Builder;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -25,14 +32,13 @@ public class MainActivity extends Activity implements OnClickListener {
 			LOGOUT_SUCCESS = 5;
 
 	final String loginUrl = "http://202.115.254.251/";
-
 	EditText accoutEditText, passwdEditText;
 	Button loginButton, logoutButton;
 	CheckBox remberCheckBox;
-
-	private String accout, passwd;
-
+	private String account, passwd;
 	MHandler mHandler = new MHandler(this);
+	SharedPreferences sp;
+	boolean rember;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +56,43 @@ public class MainActivity extends Activity implements OnClickListener {
 		loginButton.setOnClickListener(this);
 		logoutButton.setOnClickListener(this);
 
+		// 从本地记录回复保存的用户名密码
+		sp = PreferenceManager
+				.getDefaultSharedPreferences(getApplicationContext());
+		account = sp.getString("account", null);
+		passwd = sp.getString("passwd", null);
+		rember = sp.getBoolean("rember", false);
+
+		if (account != null) {
+			accoutEditText.setText(account);
+		}
+
+		if (passwd != null) {
+			passwdEditText.setText(passwd);
+		}
+
+		remberCheckBox.setChecked(rember);
+
+		remberCheckBox
+				.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+					@Override
+					public void onCheckedChanged(CompoundButton buttonView,
+							boolean isChecked) {
+						// TODO Auto-generated method stub
+						rember = isChecked;
+					}
+				});
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		Editor ed = sp.edit();
+		ed.putString("account", account);
+		ed.putString("passwd", passwd);
+		ed.putBoolean("rember", rember);
+		ed.commit();
+		super.onDestroy();
 	}
 
 	@Override
@@ -60,16 +103,30 @@ public class MainActivity extends Activity implements OnClickListener {
 	}
 
 	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		switch (item.getItemId()) {
+		case R.id.action_settings:
+			Builder builder = new Builder(MainActivity.this);
+			builder.setTitle("关于")
+					.setMessage("作者：刘亚军 @黑色之光\nEmail：liuyajun52@gmail.com")
+					.setPositiveButton("确认", null);
+			builder.create().show();
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		switch (v.getId()) {
 		case R.id.loginButton:
-			accout = accoutEditText.getText().toString();
+			account = accoutEditText.getText().toString();
 			passwd = passwdEditText.getText().toString();
 
 			// 准备要向网站传递的参数
 			Map<String, String> params = new HashMap<String, String>();
-			params.put("DDDDD", accout);
+			params.put("DDDDD", account);
 			params.put("upass", passwd);
 			params.put("0MKKey", "%B5%C7%C2%BC+Login");
 			params.put("Submit", "Login/登陆");
@@ -79,11 +136,11 @@ public class MainActivity extends Activity implements OnClickListener {
 
 			break;
 		case R.id.logoutButton:
-			accout = accoutEditText.getText().toString();
+			account = accoutEditText.getText().toString();
 			passwd = passwdEditText.getText().toString();
 			// 准备要向网站传递的参数
 			Map<String, String> params2 = new HashMap<String, String>();
-			params2.put("DDDDD", accout);
+			params2.put("DDDDD", account);
 			params2.put("upass", passwd);
 			params2.put("0MKKey", "%B5%C7%C2%BC+Login");
 			params2.put("Submit2", "Logout/注销");
@@ -123,8 +180,7 @@ public class MainActivity extends Activity implements OnClickListener {
 						.show();
 				break;
 			case LOGOUT_SUCCESS:
-				Toast.makeText(act.get(), "注销成功", Toast.LENGTH_SHORT)
-						.show();
+				Toast.makeText(act.get(), "注销成功", Toast.LENGTH_SHORT).show();
 				break;
 			}
 		}
